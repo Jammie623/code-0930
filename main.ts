75e2cdabe6d6d5884825bdd8c0fe43ce82214149
Jammie623 <71751181+Jammie623@users.noreply.github.com>
2020-09-30 15:21:33 +0800

    GPA

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let gpa = 0
+input.onGesture(Gesture.Shake, function () {
+    gpa = randint(80, 100)
+    if (gpa < 84) {
+        basic.showNumber(randint(80, 84))
+        basic.showString("A-")
+    } else if (gpa < 89) {
+        basic.showNumber(randint(85, 89))
+        basic.showString("A")
+    } else if (gpa < 100) {
+        basic.showNumber(randint(90, 100))
+        basic.showString("A+")
+    } else {
+        basic.showString("A-")
+    }
+})
 basic.forever(function () {
 	
 })
